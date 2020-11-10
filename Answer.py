d2f9895c0625cb0fd8696ae7224c9d068f6f54a2
FitriXyni <74171724+FitriXyni@users.noreply.github.com>
2020-11-10 11:28:32 +0800

    Update Answer.py

--- Answer.py
+++ Answer.py
@@ -1,11 +1,11 @@
 
 class Square
-	side = 1.0
-	def__init__(side):
-	self.side=side;
+   side = 1.0
+   def__init__(side):
+      self.side=side;
 	
-	def setSide(self):
-		if self.side < 0:
+   def setSide(self):
+    if self.side < 0:
          self.side=0
     else:
          self.side=self
